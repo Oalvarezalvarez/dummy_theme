d39b4bf3e7ee1fc524479926817c350c976bd2f6
Óscar <superoscarap@gmail.com>
2015-04-07 19:46:55 +0200

    Tema Dummy

---
+++ __openerp__.py
@@ -0,0 +1,33 @@
+{
+    # Theme information
+    'name': "Dummy Theme",
+    'description': """
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates, pages, and snippets
+    'data': [
+        'views/pages.xml',
+        #'views/options.xml',
+        #'views/snippets.xml',
+    ],
+
+    # Your information
+    'author': "Your Company",
+    'website': "",
+}
+
+#{
+#    'name': 'Dummy Theme',
+#    'description': 'Dummy Theme',
+#    'category': 'Website',
+#    'version': '1.0',
+#    'author': 'OpenERP SA',
+#    'depends': ['website'],
+#    'data': [
+#        'views/snippets.xml',
+#    ],
+#    'application': True,
+#}
